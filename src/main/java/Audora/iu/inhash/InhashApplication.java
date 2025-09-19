b0434dd0214fe1bd8fd7393fb6ad438ded986674
YeongchanChoi <gy2553@naver.com>
2025-09-19 15:00:30 +0900

    first commit

---
+++ src/main/java/Audora/iu/inhash/InhashApplication.java
@@ -0,0 +1,13 @@
+package Audora.iu.inhash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Audora/iu/inhash/InhashApplicationTests.java
@@ -0,0 +1,13 @@
+package Audora.iu.inhash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
